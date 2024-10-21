5f4b1949ac687a896a5c8a209c81f8396ef34b33
Michael Nguyen <ngxphong03@gmail.com>
2024-10-21 19:18:00 +0700

    update

--- src/app/[locale]/_components/ThemeChangerButton.jsx
+++ src/app/[locale]/_components/ThemeChangerButton.jsx
@@ -1,8 +1,6 @@
 "use client";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import { FaMoon } from "react-icons/fa";
-import { MdSunny } from "react-icons/md";
 
 export default function ThemeChangerButton() {
   const [mounted, setMounted] = useState(false);
@@ -22,12 +20,20 @@ export default function ThemeChangerButton() {
     setMounted(true);
   }, []);
 
-  if (!mounted) return <FaMoon className="text-xl" />;
+  if (!mounted)
+    return (
+      <button className="px-6 py-2 bg-gray-200 rounded-md hover:bg-gray-300 dark:bg-gray-500">
+        dark
+      </button>
+    );
 
   return (
-    <button onClick={handleSwitchTheme}>
-      {theme === "dark" && <MdSunny className="text-2xl" />}
-      {theme === "light" && <FaMoon className="text-xl" />}
+    <button
+      onClick={handleSwitchTheme}
+      className="px-6 py-2 bg-gray-200 rounded-md hover:bg-gray-300 dark:bg-gray-500"
+    >
+      {theme === "dark" && "light"}
+      {theme === "light" && "dark"}
     </button>
   );
 }
